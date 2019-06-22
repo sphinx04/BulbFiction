76de4e1f73caeb7bfe3a9a4a5648b6faf9d1d35f
Mnatsakan Zurnadzhian <mnats1997@gmail.com>
2019-06-22 21:15:57 +0300

    add bulb collector

---
+++ Assets/SizeChanger.cs
@@ -0,0 +1,29 @@
+﻿using Cinemachine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeChanger : MonoBehaviour
+{
+    public GameObject player;
+    public float defaultCameraSize;
+    private CinemachineVirtualCamera VCamera;
+    private Rigidbody2D rb;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = player.GetComponent<Rigidbody2D>();
+        VCamera = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = Mathf.Abs(rb.velocity.x) > Mathf.Abs(rb.velocity.y) ?
+        Mathf.Abs(rb.velocity.x) :
+        Mathf.Abs(rb.velocity.y) * 0.5f;
+        VCamera.m_Lens.OrthographicSize = defaultCameraSize + speed / 2;
+    }
+}
