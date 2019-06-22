2cb31fd43ef7a0b4c1916f37742d3ada512b70cc
Mnatsakan Zurnadzhian <mnats1997@gmail.com>
2019-06-23 00:52:55 +0300

    add dynamic health system

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,47 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
 
 public class PlayerController : MonoBehaviour
 {
     public SpriteRenderer sr;
+    public float DefaultEnergy = 100;
     public float speed;
     public float jumpForce;
-    private float moveInput;
-
-    private Rigidbody2D rb;
-
-    private bool isGrounded;
     public Transform groundCheck;
     public float checkRadius;
     public LayerMask whatIsGround;
-
-    private int extraJumps;
     public int extraJumpsValue;
+    [Range(0f, 2f)]
+    public float defaultIntensity;
+    [Range(0f, 1f)]
+    public float additionalIntensity;
+
+
+    private float moveInput;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private int extraJumps;
+    private Light2D playerLight;
+    private float currentIntensity;
+    private float currentEnergy;
+
 
     void Start()
     {
         extraJumps = extraJumpsValue;
         rb = GetComponent<Rigidbody2D>();
+        playerLight = GetComponent<Light2D>();
+        currentIntensity = defaultIntensity;
+        playerLight.intensity = currentIntensity;
+        currentEnergy = DefaultEnergy;
     }
 
     private void Update()
     {
-        if (isGrounded == true)
+        if (currentEnergy <= 0f)
         {
-            extraJumps = extraJumpsValue;
+            Time.timeScale = 0;
         }
-
-        if (Input.GetKeyDown(KeyCode.UpArrow) && extraJumps > 0)
+        else
         {
-            rb.velocity = Vector2.up * jumpForce;
-            extraJumps--;
-        }
-        else if (Input.GetKeyDown(KeyCode.UpArrow) && extraJumps == 0 && isGrounded == true)
-        {
-            rb.velocity = Vector2.up * jumpForce;
+            float tmpInt = ChangeIntensity();
+            currentEnergy -= tmpInt * 0.01f;
+            playerLight.intensity = (defaultIntensity + tmpInt) * currentEnergy / DefaultEnergy;
+            PlayerControl();
         }
     }
+
     void FixedUpdate()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
@@ -60,8 +71,43 @@ public class PlayerController : MonoBehaviour
             sr.flipX = false;
         }
     }
+
     public void OnOff()
     {
         gameObject.SetActive(gameObject.activeSelf ? false : true);
+    }
+
+    float ChangeIntensity()
+    {
+        return Mathf.Abs(rb.velocity.x) > Mathf.Abs(rb.velocity.y) ?
+Mathf.Abs(rb.velocity.x) * additionalIntensity * defaultIntensity :
+Mathf.Abs(rb.velocity.y) * 0.5f * additionalIntensity * defaultIntensity;
+    }
+
+    void PlayerControl()
+    {
+        if (isGrounded == true)
+        {
+            extraJumps = extraJumpsValue;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && extraJumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow) && extraJumps == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Bulb")
+        {
+            collision.gameObject.SetActive(false);
+            currentEnergy = DefaultEnergy;
+        }
     }
 }
