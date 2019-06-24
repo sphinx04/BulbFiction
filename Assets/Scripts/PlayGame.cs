2b68042f537054892da32a8bb5e19666a0301a7a
Sphinx04 <mnats1997@gmail.com>
2019-06-24 17:58:43 +0300

    Glow Particles and post-processing

---
+++ Assets/Scripts/PlayGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayGame : MonoBehaviour
+{
+    public GameObject playText;
+    public GameObject settingsText;
+    public GameObject quitText;
+    public GameObject playParticles;
+    public GameObject settingsParticles;
+    public GameObject quitParticles;
+
+    public void StartGame()
+    {
+        playText.SetActive(false);
+        settingsText.SetActive(false);
+        quitText.SetActive(false);
+        playParticles.SetActive(true);
+        settingsParticles.SetActive(true);
+        quitParticles.SetActive(true);
+    }
+
+}
