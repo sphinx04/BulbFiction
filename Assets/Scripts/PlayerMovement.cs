6e65d674af30baf3dcef81835ae22f68f379ebb0
Mnatsakan Zurnadzhian <mnats1997@gmail.com>
2020-01-14 01:50:48 +0300

    UI bugfix

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public CharacterController2D controller;
+
+	public float runSpeed = 40f;
+
+	float horizontalMove = 0f;
+	bool jump = false;
+	bool crouch = false;
+
+    private void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+    // Update is called once per frame
+    void Update () {
+
+        //horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        //horizontalMove = joystick.Horizontal * runSpeed;
+
+
+
+
+
+        if (Input.GetAxisRaw("Horizontal") > 0f || CrossPlatformInputManager.GetAxis("Horizontal") > .2f)
+        {
+            horizontalMove = runSpeed;
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0f || CrossPlatformInputManager.GetAxisRaw("Horizontal") < -.2f)
+        {
+            horizontalMove = -runSpeed;
+        }
+        else
+        {
+            horizontalMove = 0;
+        }
+
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+
+
+    //void FixedUpdate ()
+    //{
+    //	// Move our character
+    //	controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+    //	jump = false;
+    //}
+}
